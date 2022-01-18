61c811a591c8fe4ab00adfc944805ccc1bb7d941
danugans <mas.danu29@gmail.com>
2022-01-18 10:27:10 +0700

    recent

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
 //allow this app to receive incoming json request
 //Create app.use for express.json here
+app.use(express.json());
 
 let todos = [
-    {
-        id: 1,
-        title: "Cuci tangan",
-        isDone: true
-    },
-    {
-        id: 2,
-        title: "Jaga jarak",
-        isDone: false
-    },
-]
+  {
+    id: 1,
+    title: "Cuci tangan",
+    isDone: true,
+  },
+  {
+    id: 2,
+    title: "Jaga jarak",
+    isDone: false,
+  },
+];
 
 //GET list route: simply send arr of obj todos on your user screen
 // Create method GET here
+app.get("/todos", (req, res) => {
+  res.send({ data: todos });
+});
 
 //GET detail route: send the todo obj, by received id request params
 // Create method GET by received id here
+app.get("/todo/:id", (req, res) => {
+  const id = req.params.id;
+  const index = id - 1;
+  res.send(todos[index]);
+
+  //const data = todos.find(item)
+});
 
 //POST route: receive json body request, from user input, then push to todos array
 // Create method POST here
+app.post("/todo", (req, res) => {
+  todos = [...todos, req.body];
+  res.send({ data: todos });
+});
 
 //PATCH route: receive json body request, from user input, then push to todos array
 //by object id
 // Create method PATCH here
+app.patch("/todo/:id", (req, res) => {
+  const { id } = req.params;
+
+  todos[id - 1] = { ...todos[id - 1], ...req.body };
+
+  res.send({ data: todos[id - 1] });
+});
 
 //DELETE route: delete the todo obj, by received id request params
 // Create method DELETE here
+app.delete("/todo/:id", (req, res) => {
+  const { id } = req.params;
+  todos = todos.filter((todo) => todo.id != id);
+  res.send({ data: todos });
+});
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`));
